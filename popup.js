0f20fb767b31086be6a32b5e3cfaf27dcff65f97
tussakin <131391869+tussakin@users.noreply.github.com>
2024-01-24 14:21:59 +0100

    All code up to this point

---
+++ popup.js
@@ -0,0 +1,40 @@
+function togglePopup(popupId) {
+    var popup = document.getElementById(popupId);
+    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';
+}
+
+function closeGallery() {
+    var popups = document.querySelectorAll('.popup');
+    popups.forEach(function (popup) {
+        popup.style.display = 'none';
+    });
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Escape") {
+        closeGallery();
+    }
+}
+
+document.addEventListener("keydown", handleKeyPress);
+
+
+$(function(){
+    var url = window.location.href; 
+    $("#sub-header a").each(function() {
+        if(url == (this.href)) { 
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var currentPage = window.location.href;
+    var navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(function (link) {
+        if (link.href === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
